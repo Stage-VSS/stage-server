9386595c0097dca4c0a1cd10d6148491ff97ae0c
Mark Cafaro <mark.cafaro@gmail.com>
2015-11-04 13:03:57 -0800

    Changed build location

--- src/main/matlab/+stagenet/+app/App.m
+++ src/main/matlab/+stagenet/+app/App.m
@@ -3,7 +3,7 @@ classdef App < handle
     methods (Static)
         
         function v = version()
-            v = '1.0.1.1'; % i.e. 1.0-b1
+            v = '2.0.1.1'; % i.e. 2.0-b1
         end
         
     end
