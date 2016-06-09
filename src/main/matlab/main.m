3d40d5ad39871da8d5c2f820116dcae471c6b35e
Mark Cafaro <mark.cafaro@gmail.com>
2016-06-09 14:35:20 -0700

    Added git updater

--- src/main/matlab/main.m
+++ src/main/matlab/main.m
@@ -1,5 +1,17 @@
 function main()
+    busy = appbox.BusyPresenter('This may take a moment.', 'Starting...');
+    busy.go();
+    deleteBusy = onCleanup(@()delete(busy));
+    
+    updater = appbox.GitHubUpdater();
+    isUpdate = updater.checkForUpdates(stage.app.App.owner, stage.app.App.repo, struct('name', stage.app.App.name, 'version', stage.app.App.version));
+    if isUpdate
+        appbox.UpdatePresenter(updater).goWaitStop();
+    end
+
     presenter = stageui.ui.presenters.MainPresenter();
+    
+    delete(busy);
     presenter.go();
 end
 
