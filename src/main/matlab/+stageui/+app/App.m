ee275ff39199e89bf211291b7bcc06e9c07cf919
Mark Cafaro <mark.cafaro@gmail.com>
2016-06-09 16:30:43 -0700

    Switched back to old versioning system

--- src/main/matlab/+stageui/+app/App.m
+++ src/main/matlab/+stageui/+app/App.m
@@ -3,7 +3,7 @@ classdef App < handle
     methods (Static)
         
         function v = version()
-            v = stage.app.App.version;
+            v = '2.0.3.3'; % i.e. 2.0-r3
         end
         
     end
