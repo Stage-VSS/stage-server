9bd893662b5f6d691d3eaf80470a812d1fb0dd67
Mark Cafaro <mark.cafaro@gmail.com>
2016-07-31 17:55:31 -0700

    Updated version number

--- src/main/matlab/+stageui/+app/App.m
+++ src/main/matlab/+stageui/+app/App.m
@@ -3,7 +3,7 @@ classdef App < handle
     methods (Static)
         
         function v = version()
-            v = '2.0.3.3'; % i.e. 2.0-r3
+            v = '2.1.3.0'; % i.e. 2.1-r
         end
         
     end
