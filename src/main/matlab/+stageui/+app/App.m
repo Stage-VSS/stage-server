7f3fc357547c64e0e255231107eb12e9ba0d2fed
Mark Cafaro <mark.cafaro@gmail.com>
2017-01-06 14:10:15 -0800

    Preparing for release

--- src/main/matlab/+stageui/+app/App.m
+++ src/main/matlab/+stageui/+app/App.m
@@ -3,7 +3,7 @@ classdef App < handle
     methods (Static)
         
         function v = version()
-            v = '2.2.3.0'; % i.e. 2.2-r
+            v = '2.3.3.0'; % i.e. 2.3-r
         end
         
     end
